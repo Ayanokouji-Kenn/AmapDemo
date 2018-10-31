de8766702ed19162e4a31510173fe3158e78b2b8
xuzijian <xuzijian@zifangdt.com>
2018-10-31 15:22:37 +0800

    Initial commit

---
+++ app/src/main/java/com/zifangdt/amapdemo/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.zifangdt.amapdemo
+
+import android.arch.lifecycle.ViewModelProviders
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var vm:MainVm
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        vm = ViewModelProviders.of(this).get(MainVm::class.java)
+        map.onCreate(savedInstanceState)
+        vm.initMyLocation(map.map)
+
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        map.onSaveInstanceState(outState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        map.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        map.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        map.onDestroy()
+    }
+}
